a5c41e9a6d2c7481c85297e126bf6dde5f74ae27
anuraag <anuraag5066@gmail.com>
2026-06-01 22:35:40 +0530

    style changes

--- api/index.py
+++ api/index.py
@@ -8,6 +8,7 @@ app = FastAPI()
 @app.get("/api", response_class=PlainTextResponse)
 def idea():
     client = OpenAI()
-    prompt = [{"role": "user", "content": "Come up with a new business idea for AI Agents"}]
+    prompt = [{"role": "user", 
+               "content": "Reply with a new business idea for AI Agents, formatted with headings, sub-headings and bullet points"}]
     response = client.chat.completions.create(model="gpt-5-nano", messages=prompt)
     return response.choices[0].message.content
